f254ff8d23a34ab5190ed0246fa3d43a9968ca7c
aliqureshi777 <aliqureshi777@gmail.com>
2024-04-06 11:45:05 +0500

    push my calculator to git hub

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter Second number", type: "number", name: "secondNumber" },
+    {
+        message: "Enter Operator",
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "/", "*"],
+    },
+]);
+if (answer.operator === "+") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "-") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "*") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "/") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select valid operator");
+}
